30e4cf1388d2e5456449c2f42b0dd5322bb11dcc
ChisBread <chisbread@gmail.com>
2025-12-12 22:46:50 +0800

    Fix build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller  --add-data ".\img\icon-cheese-32.ico:.\img\" --onefile --windowed --icon .\img\icon-cheese-32.ico .\chismaker.py
+pyinstaller --add-data "img\icon-cheese-32.ico;img\" --onefile --windowed --icon img\icon-cheese-32.ico chismaker.py
